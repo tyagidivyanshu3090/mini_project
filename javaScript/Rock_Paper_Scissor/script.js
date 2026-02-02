6b2ca5bbb8035b48426a32a7086bb2b56024c99b
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2026-02-02 22:55:36 +0530

    The Rock 🪨, Paper 🧻, or Scissors ✂️ Game

---
+++ javaScript/Rock_Paper_Scissor/script.js
@@ -0,0 +1,16 @@
+/**
+ * The Rock 🪨, Paper 🧻, or Scissors ✂️ Game
+ * This game will be played between the computer and human. Once Human will select Rock, Paper, or Scissors and in the next turn the computer will select Rock, Paper, or Scissors.
+ * The Winner will be selected based on these rules:
+ * - Rock can Break Scissors => Rock Wins
+ * - Paper can wrap Rock => paper Wins
+ * - Scissors can cut Paper => Scissors Wins
+ * Ask the input from user and randomly select a computer selection to continue the game.
+ */
+
+// Assumptions:
+// 1. We have to prompt the user to get their inputs.
+// 2. The Computer's selection will be random.
+// 3. We have to compare user and computer's choice
+// 4. We need to announce the winner.
+// 5. After the Winner announcement, we may want to ask the user to play again or quit from the game.
