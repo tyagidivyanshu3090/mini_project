8975f90ce5d1f1ad1dfd0227668a9962b6e03151
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2026-02-08 00:02:46 +0530

    feat: Add a boundary check to prevent the counter from going below zero.

--- React/Counter_Application/counter-app/src/App.jsx
+++ React/Counter_Application/counter-app/src/App.jsx
@@ -1,19 +1,39 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  function decrement() {
+    if (count === 0) return;
+    setCount(count - 1);
+  }
 
   return (
     <>
-    <h1>Counter Application</h1>
-    <p> Count: {count}</p>
-    <button onClick={() => setCount(count + 1)} style ={{color:"green", padding:"10px", margin:"10px"}}>➕ Increment</button>
-    <button onClick={() => setCount(count - 1)} style ={{color:"red", padding:"10px", margin:"10px"}}>➖ Decrement</button>
-    <button onClick={() => setCount(0)} style ={{color:"blue", padding:"10px", margin:"10px"}}>🔄 Reset</button>
+      <h1>Counter Application</h1>
+      <p> Count: {count}</p>
+      <button
+        onClick={() => setCount(count + 1)}
+        style={{ color: "green", padding: "10px", margin: "10px" }}
+      >
+        ➕ Increment
+      </button>
+      <button
+        onClick={decrement}
+        style={{ color: "red", padding: "10px", margin: "10px" }}
+      >
+        ➖ Decrement
+      </button>
+      <button
+        onClick={() => setCount(0)}
+        style={{ color: "blue", padding: "10px", margin: "10px" }}
+      >
+        🔄 Reset
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
