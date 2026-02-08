99d3f47d60876a9a33cb3e6fd47d41eef462b31e
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2026-02-09 01:10:51 +0530

    feat: Add a basic root route to display a welcome message.

--- Backend/DevTinder/src/app.js
+++ Backend/DevTinder/src/app.js
@@ -5,6 +5,10 @@ const app = express();
 
 const PORT = 3000;
 
+app.use("/", (req, res) => {
+  res.send("Welcome to life of backend");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
