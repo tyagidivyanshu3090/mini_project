dd938fc1f110e2f7764d976eba1bc21b7ef798aa
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2026-02-12 01:24:51 +0530

    feat: Add global authorization middleware to validate a query key and remove several unused route handlers.

--- Backend/DevTinder/src/app.js
+++ Backend/DevTinder/src/app.js
@@ -1,55 +1,29 @@
-// Creating the server
-
 const express = require("express");
 const app = express();
 
 const PORT = 3000;
 
-// Route Handler for GET user: Getting the user
+// Implementing the middleware using the app.use()
+const KEY = "xyz";
+
+app.use((req, res, next) => {
+  const { key } = req.query;
+
+  if (key === KEY) {
+    next();
+  } else {
+    res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+});
+
+// Implementing the route handler
 app.get("/user", (req, res) => {
   res.status(200).json({
     name: "Divyanshu",
     age: 21,
     city: "Delhi",
-  });
-});
-
-// Handling multiple route handlers for a GET request
-app.get(
-  "/user/status",
-  (req, res, next) => {
-    // This function acts as a 'gatekeeper' or 'logger'.
-    console.log("Middleware executed: Checking conditions...");
-
-    // IMPORTANT: next() is called to pass control to the second function.
-    // If you remove next(), the request will hang and never reach the next handler.
-    next();
-  },
-
-  // 3. The Final Route Handler (Request Handler)
-  (req, res) => {
-    // This function is responsible for ending the request-response cycle.
-    // It sends the final data back to the client.
-    res.status(200).json({
-      message: "User status",
-    });
-  },
-);
-
-// Route Handler for Post User: Creating the user
-app.post("/user", (req, res) => {
-  res.status(200).json({
-    message: "User created successfully",
-  });
-});
-
-// Route Handler for Delete User: Deleting the user
-// :id is the dynamic parameter -> query parameter
-// Api -> http://localhost:3000/user/:21
-app.delete("/user/:id", (req, res) => {
-  const { id } = req.params;
-  res.status(200).json({
-    message: `User with id ${id} deleted successfully`,
   });
 });
 
