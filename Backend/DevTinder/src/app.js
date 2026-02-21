06b4c9df4929d027e358139afb59f03f7c9e1f9e
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2026-02-21 14:04:31 +0530

    feat: add global error handling middleware.

--- Backend/DevTinder/src/app.js
+++ Backend/DevTinder/src/app.js
@@ -22,6 +22,14 @@ app.delete("/admin/users", (req, res) => {
   });
 });
 
+// Implementing the error handler
+app.use("/", (err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "Internal server error",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
