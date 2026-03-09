56cc6896d3984f662213134b45df4611322bf935
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2026-03-09 23:04:56 +0530

    feat: Implement MongoDB connection using Mongoose and configure database setup.

--- Backend/DevTinder/src/app.js
+++ Backend/DevTinder/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const adminAuth = require("./middleware/adminAuth");
+require("./config/database");
 
 const PORT = 3000;
 
--- Backend/DevTinder/src/config/database.js
+++ Backend/DevTinder/src/config/database.js
@@ -1,1 +1,18 @@
 // Logic for connecting to database: Mongoose library is used
+
+const mongoose = require("mongoose");
+// It is best practice to store this in a .env file
+const mongoURI =
+  "mongodb+srv://devTinder:Divyanshu3090%40@cluster0.3wshelt.mongodb.net/devTinder";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("✅ Connected to MongoDB Atlas");
+  } catch (error) {
+    console.error("❌ Connection error:", error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
