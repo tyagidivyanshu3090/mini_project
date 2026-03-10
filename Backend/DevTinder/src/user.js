f026b4ca3467ceb17bb70f0337082ee42affc020
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2026-03-11 05:25:23 +0530

    feat: Implement user schema with firstName, lastName, password, email, age, and gender fields, including validation.

--- Backend/DevTinder/src/user.js
+++ Backend/DevTinder/src/user.js
@@ -1,3 +1,25 @@
 const moongose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true, // Mandatory field
+  },
+  lastName: String, // Defining the type of the variable directly
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    min: 18, // Defining the minimum value of the variable
+  },
+  gender: {
+    type: String,
+  },
+});
