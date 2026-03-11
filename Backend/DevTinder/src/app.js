70b7f6690c700398ea0725a81f92eabc0e8bb2d7
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2026-03-11 06:36:16 +0530

    feat: Add signup route for user creation and correct mongoose import in user model.

--- Backend/DevTinder/src/app.js
+++ Backend/DevTinder/src/app.js
@@ -2,8 +2,28 @@ const express = require("express");
 const app = express();
 const adminAuth = require("./middleware/adminAuth");
 const { connectDB } = require("./config/database");
+const UserModel = require("./models/user");
 
 const PORT = 3000;
+
+// Creating the post Route handler to save data to database
+app.post("/signup", async (req, res) => {
+  // Creating the dummy data:
+  const userObject = {
+    firstName: "Divyanshu",
+    lastName: "Tyagi",
+    email: "divyanshu422@gmail.com",
+    password: "[PASSWORD]",
+    age: 29,
+    gender: "Male",
+  };
+  // Creating the instance of UserModel to save data to database with userObject
+  const user = new UserModel(userObject);
+  // Saving the data to database
+  await user.save();
+  // Sending the response to the client
+  res.status(201).json({ message: "User created successfully" });
+});
 
 const startServer = async () => {
   try {
--- Backend/DevTinder/src/models/user.js
+++ Backend/DevTinder/src/models/user.js
@@ -1,4 +1,4 @@
-const moongose = require("mongoose");
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   firstName: {
