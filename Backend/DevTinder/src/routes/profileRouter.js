c381049fab6326e2d4e09dd68efdc7c39451d784
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2026-03-21 22:05:11 +0530

    refactor: move user deletion and update routes from app.js to profileRouter.js

--- Backend/DevTinder/src/routes/profileRouter.js
+++ Backend/DevTinder/src/routes/profileRouter.js
@@ -32,4 +32,70 @@ profileRouter.get("/all-user", authMiddleware, async (req, res) => {
   }
 });
 
+profileRouter.delete("/deleteuser", async (req, res) => {
+  try {
+    const email = req.query.email;
+    const userFind = await UserModel.findOne({ email: email });
+    if (userFind === null) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const user = await UserModel.deleteOne({ email: email });
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (err) {
+    console.error("Error deleting user", err);
+    res.status(500).json("Error deleting user");
+  }
+});
+
+profileRouter.patch("/updateuser", async (req, res) => {
+  try {
+    const userId = req.query.id;
+    const data = req.body;
+    const ALLOWED_UPDATE_FIELDS = [
+      "firstName",
+      "lastName",
+      "age",
+      "gender",
+      "photoUrl",
+      "about",
+      "skills",
+    ];
+
+    const isUpdateAllowed = Object.keys(data).every((key) => {
+      return ALLOWED_UPDATE_FIELDS.includes(key);
+    });
+
+    const unAllowedFields = Object.keys(data).filter((key) => {
+      return !ALLOWED_UPDATE_FIELDS.includes(key);
+    });
+
+    if (unAllowedFields.length > 0) {
+      return res
+        .status(400)
+        .json({ message: "Invalid update fields", error: unAllowedFields });
+    }
+
+    if (!isUpdateAllowed) {
+      return res.status(400).json({ message: "Invalid update fields" });
+    }
+    if (req.body.skills.length > 10) {
+      return res
+        .status(400)
+        .json({ message: "Skills cant be greater than 10" });
+    }
+
+    const user = await UserModel.findByIdAndUpdate(userId, req.body, {
+      new: true,
+      runValidators: true, // This will run the validators defined in the schema
+    });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User updated successfully" });
+  } catch (err) {
+    console.error("Error updating user", err);
+    res.status(500).json("Error updating user");
+  }
+});
+
 module.exports = profileRouter;
